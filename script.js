1f8c45ec95141b772dbab7521159e88f7070d96b
Raven <65698621+orourkeraven@users.noreply.github.com>
2020-06-17 14:57:01 -0600

    canvas and onclicks working

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
+let currentSlide = 0;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
-let currentSlide = 0;
-
-function changeText() {
-    //create function that changes between slides. slide number will be a variable
-    document.getElementById("h3text").firstChild.nodeValue=String(currentSlide);
-    return true;
-}
-
 function switchSlide(x) {
-    switch (x){
+    switch (x) {
         case 'L':
-            if (currentSlide >= 0){
-                document.getElementById("slidetext").firstChild.nodeValue="Previous Slide";
-                currentSlide -= 1;
-            }
+            document.getElementById("slidetext").firstChild.nodeValue = "Previous Slide";
+            currentSlide -= 1;
             break;
         case 'R':
             //check that not at end
-            document.getElementById("slidetext").firstChild.nodeValue="Next Slide";
+            document.getElementById("slidetext").firstChild.nodeValue = "Next Slide";
             currentSlide += 1;
             break;
         default:
@@ -28,5 +18,12 @@ function switchSlide(x) {
             break;
 
     }
+    return true;
+}
+
+function changeText() {
+    //create function that changes between slides. slide number will be a variable
+    document.getElementById("h3text").style.color="red";
+    document.getElementById("h3text").innerHTML=String(currentSlide);
     return true;
 }
